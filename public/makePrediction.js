77730a8109e85bc95767e9e524bce7cd202bdf2c
JadenB1535 <114974929+JadenB1535@users.noreply.github.com>
2023-04-04 13:44:16 +0800

    Fixed loading page

--- public/makePrediction.js
+++ public/makePrediction.js
@@ -1,16 +1,16 @@
 async function predict() {
-
-
-
-    await showLoading();
     // now lets make the prediction, we use .then because the model is a promise
     // (this is confusing as a Python user, but useful so check it out if interested)
-    await model.then(model => {
+    // switch to loading screen
+    window.location.href = "loading.html";
+
+    await model.then(async model => {
+
         // making the prediction
         const result = model.predict(inputTensorBatched);
 
-        // alerting the result
-        selection = num_classes[tf.argMax(result, 1).dataSync()[0]];
+        // storing result in session storage
+        sessionStorage.setItem("Vegetable", num_classes[tf.argMax(result, 1).dataSync()[0]]);
         //alert(selection);
 
         // disposing of all tensors
@@ -19,27 +19,10 @@ async function predict() {
         inputTensorResized.dispose();
         inputTensorBatched.dispose();
 
-        // get()
-        //     .catch(err => console.log(err));
+        
     });
 
-    await hideLoading();
-
+    //switch back to homescreen
+    //window.location.href = "index.html";
 };
 
-async function showLoading() {
-    //console.log("Helo");
-    // showing loading screen
-    document.getElementById("analysing").innerHTML = "Analyzing...please wait";
-    // making button disabled while predicting
-    document.getElementById("clickable-overlay").disabled = true;
-}
-
-async function hideLoading() {
-    //console.log("Hel1o");
-
-    // remove loading screen
-    document.getElementById("p1").innerHTML = "Get started";
-    // making button enabled after predicting
-    document.getElementById("clickable-overlay").disabled = false;
-}
--- public/sendVegSelection.js
+++ public/sendVegSelection.js
@@ -2,7 +2,7 @@ async function post() {
     // sending post request
     const response = await fetch('../sendVegSelection', {
         method: "POST",
-        body: JSON.stringify({ vegetableName: selection }),
+        body: JSON.stringify({ vegetableName: sessionStorage.getItem("Vegetable") }),
         headers: new Headers({ "Content-Type": "application/json" }),
     });
     const json = await response.json();
@@ -17,9 +17,8 @@ async function get() {
         headers: new Headers({ "Content-Type": "application/json" }),
     });
     const json = await response.json();
-
     json.forEach(i => {
-        if (i.vegetableName == selection) {
+        if (i.vegetableName == sessionStorage.getItem("Vegetable")) {
             result = false;
             alert("Vegetable already added. Try another vegetable.");
         }
@@ -27,7 +26,13 @@ async function get() {
     if (result) {
         post()
             .catch(err => console.log(err));
+        sessionStorage.clear();
         alert("Vegetable successfully added!");
-
     }
+    
+    window.location.href = "index.html";
 }
+
+
+get()
+    .catch(err => console.log(err));
